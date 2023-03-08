c33c6481d41ac9c80e412645e58c241725bc65f4
nine0426 <1016802678@qq.com>
2023-03-08 21:56:01 +0800

    更新 jd.js

--- js/jd.js
+++ js/jd.js
@@ -1,1 +1,1 @@
-copyToBoard('https://rss.mofast.buzz/api/v1/client/subscribe?token=5fa63f2eaa46a34bf87543fd0dd0b9eb')
+copyToBoard("https://rss.mofast.buzz/api/v1/client/subscribe?token=5fa63f2eaa46a34bf87543fd0dd0b9eb");
