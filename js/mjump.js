52f0a2e888b5e9b82b8dec02a737d0badf39b090
nine0426 <1016802678@qq.com>
2023-03-15 19:48:21 +0800

    微信识别+路径优化

---
+++ js/mjump.js
@@ -0,0 +1,4 @@
+if (ua.match(/MicroMessenger/i) == "micromessenger") {
+    //在微信中打开
+    window.location = window.location.href.replace('https://nine0426.github.io/m.html', 'https://nine0426.github.io/wx.html');
+}
