3d73bc1ccb482b3ed9f82e4b0bb40b943361a83a
nine0426 <1016802678@qq.com>
2023-03-15 19:59:39 +0800

    更新 jump.js

--- js/jump.js
+++ js/jump.js
@@ -1,6 +1,4 @@
 var device = navigator.userAgent.toLowerCase();
 if (/ipad|iphone|midp|rv:1.2.3.4|ucweb|android|windows ce|windows mobile/.test(device)) {
     window.location = window.location.href.replace('https://nine0426.github.io/', 'https://nine0426.github.io/m.html');
-} else {
-    return 0;
 }
