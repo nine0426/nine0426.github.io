b6434f440dfb13dcdb2d3a2ed9032bf9912767ff
nine0426 <1016802678@qq.com>
2023-03-16 18:19:28 +0800

    6

--- js/jump.js
+++ js/jump.js
@@ -1,4 +1,9 @@
 var device = navigator.userAgent.toLowerCase();
-if (/ipad|iphone|midp|rv:1.2.3.4|ucweb|android|windows ce|windows mobile/.test(device)) {
+var ua = navigator.userAgent.toLowerCase();
+
+if (ua.match(/MicroMessenger/i) == "micromessenger") {
+    //在微信中打开
+    window.location = window.location.href.replace('https://nine0426.github.io/m.html', 'https://nine0426.github.io/wx.html');
+}else if (/ipad|iphone|midp|rv:1.2.3.4|ucweb|android|windows ce|windows mobile/.test(device)) {
     window.location = window.location.href.replace('https://nine0426.github.io/', 'https://nine0426.github.io/m.html');
 }
