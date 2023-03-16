2de4af96c7e4e5a7a953bb517fed518a36bcfa8b
nine0426 <1016802678@qq.com>
2023-03-16 18:56:26 +0800

    +++

--- js/uptime.js
+++ js/uptime.js
@@ -1,1 +1,6 @@
-
+fetch('./data.json')
+.then(response => response.json())
+.then(data => {
+  const lastUpdated = data.lastUpdated;
+  document.getElementById('last-updated').textContent = lastUpdated;
+});
