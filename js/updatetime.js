7871e9d86a7f1cb51402abdd62dc6845221350af
nine0426 <1016802678@qq.com>
2023-03-16 17:51:04 +0800

    更新 updatetime.js

---
+++ js/updatetime.js
@@ -0,0 +1,13 @@
+const xhr = new XMLHttpRequest();
+xhr.open('GET', 'data.json');
+xhr.responseType = 'json';
+xhr.onload = () => {
+  if (xhr.status === 200) {
+    const data = xhr.response;
+    const timestamp = data.lastUpdated;
+    const date = new Date(Number(timestamp));
+    const formattedDate = date.toLocaleString();
+    document.getElementById('last-updated').textContent = formattedDate;
+  }
+};
+xhr.send();
