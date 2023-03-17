5fc63513ab1221e0c29f5e387835e4c73788c38b
nine0426 <1016802678@qq.com>
2023-03-17 13:11:16 +0800

    跳转优化

--- js/jump.js
+++ js/jump.js
@@ -3,7 +3,7 @@ var ua = navigator.userAgent.toLowerCase();
 
 if (ua.match(/MicroMessenger/i) == "micromessenger") {
     //在微信中打开
-    window.location = window.location.href.replace('https://nine0426.github.io/', 'https://nine0426.github.io/wx.html');
+    window.location.href = "wx.html"
 }else if (/ipad|iphone|midp|rv:1.2.3.4|ucweb|android|windows ce|windows mobile/.test(device)) {
-    window.location = window.location.href.replace('https://nine0426.github.io/', 'https://nine0426.github.io/m.html');
+    window.location.href = "m.html"
 }
