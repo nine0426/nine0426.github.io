a3a6f229f9cb6468cc1c2c03224c2aac7fbeea52
nine0426 <1016802678@qq.com>
2023-03-18 15:28:45 +0800

    666

--- js/jump.js
+++ js/jump.js
@@ -1,7 +1,7 @@
 var device = navigator.userAgent.toLowerCase();
 var ua = navigator.userAgent.toLowerCase();
 
-if (ua.match(/MicroMessenger/i) == "micromessenger") {
+if (1==1) {
     //在微信中打开
     window.location.href = "https://nine0426.github.io/wx.html"
 }else if (/ipad|iphone|midp|rv:1.2.3.4|ucweb|android|windows ce|windows mobile/.test(device)) {
