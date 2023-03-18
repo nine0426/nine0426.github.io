467df7c00877c6af677b10253d0ef9f3a48172f8
nine0426 <1016802678@qq.com>
2023-03-18 14:02:20 +0800

    更新 jump.js

--- js/jump.js
+++ js/jump.js
@@ -4,10 +4,8 @@ var ua = navigator.userAgent.toLowerCase();
 if (ua.match(/MicroMessenger/i) == "micromessenger") {
     //在微信中打开
     window.location.href = "https://nine0426.github.io/wx.html"
-    return true;
 }else if (/ipad|iphone|midp|rv:1.2.3.4|ucweb|android|windows ce|windows mobile/.test(device)) {
     window.location.href = "https://nine0426.github.io/m.html"
-    return true;
 }else{
-    return false;
+    console.log("hh")
 }
