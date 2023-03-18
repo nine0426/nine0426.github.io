4beef5efddb385cbdaf0dd2923388430afccf7d7
nine0426 <1016802678@qq.com>
2023-03-18 17:17:47 +0800

    555

---
+++ js/disable-copy.js
@@ -0,0 +1,17 @@
+// 禁止选择文本
+document.body.style.userSelect = 'none';
+
+// 禁止拖拽元素
+document.addEventListener('dragstart', function(event) {
+  event.preventDefault();
+});
+
+document.addEventListener('copy', function(e) {
+  e.preventDefault();
+  alert("禁止复制！");
+});
+
+// 禁止鼠标右键
+document.oncontextmenu = function () {
+  return false;
+};
