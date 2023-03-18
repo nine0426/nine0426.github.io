aadd8271449a8d288ff7e23978cc1020d5697faa
nine0426 <1016802678@qq.com>
2023-03-18 13:27:42 +0800

    +

--- js/jump.js
+++ js/jump.js
@@ -3,7 +3,11 @@ var ua = navigator.userAgent.toLowerCase();
 
 if (ua.match(/MicroMessenger/i) == "micromessenger") {
     //在微信中打开
-    window.location.href = "wx.html"
+    window.location.href = "https://nine0426.github.io/wx.html"
+    return true;
 }else if (/ipad|iphone|midp|rv:1.2.3.4|ucweb|android|windows ce|windows mobile/.test(device)) {
-    window.location.href = "m.html"
+    window.location.href = "https://nine0426.github.io/m.html"
+    return true;
+}else{
+    return false;
 }
